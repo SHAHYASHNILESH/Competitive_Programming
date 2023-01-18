7786222d5327b1dcbab5b54f0afa5854560a07e2
SHAHYASHNILESH <91772554+SHAHYASHNILESH@users.noreply.github.com>
2023-01-18 11:25:20 +0530

    Create City_Tour.cpp

---
+++ City_Tour.cpp
@@ -0,0 +1,20 @@
+//City Tour
+
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+   int t;
+   cin >> t;
+   
+   while(t--){
+       
+       int n;
+       cin >> n;
+       cout << 4*n <<endl;
+   }
+   
+   return 0;
+}
